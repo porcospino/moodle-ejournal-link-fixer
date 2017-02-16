7e097eec2a4cf9fa023bb516c29ea096dd528273
Chris Fryer <c.j.fryer@lse.ac.uk>
2017-02-16 12:17:07 +0000

    Add $plugin->component
    Required since Moodle 3.0

--- version.php
+++ version.php
@@ -27,4 +27,4 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2012032700;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2010112400;        // Requires this Moodle version
-//$plugin->component = 'block_gate_adder';  // Full name of the plugin (used for diagnostics)
+$plugin->component = 'block_gate_adder';  // Full name of the plugin (used for diagnostics)
