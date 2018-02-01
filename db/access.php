3c1651ea7463ad26644234f26ae20e334d455fde
Chris Fryer <c.j.fryer@lse.ac.uk>
2018-02-01 11:32:09 +0000

    Define addinstance capability

---
+++ db/access.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * ejournal link fixer block caps.
+ *
+ * @package    block_gate_adder
+ * @copyright  Steve Bond <s.bond1@lse.ac.uk>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+
+    'block/gate_adder:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    ),
+);
+
